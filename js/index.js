fedd88d83e9a7ddbe8d987b3423a1f8a83735253
jonathan4747 <harold307lat@gmail.com>
2022-01-07 19:58:53 -0500

    eventos con onclick

---
+++ js/index.js
@@ -0,0 +1,10 @@
+console.log("Hola desde la consola");
+function alertaTexto(elemento){
+    alert("Nija was liked.");
+}
+function cambioTexto(elemento){
+    elemento.innerText="Logout";
+}
+function eliminarTexto(elemento){
+    elemento.remove();
+}
